b4ce061ec1d60ee8afd7cf4c69b7efd8aeee0bb0
Unknown <tommaso.loiacono01@gmail.com>
2017-11-24 18:07:02 +0100

    Everything

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+
+    public GameObject otherPlayerHead;
+    public Camera playerCamera;
+
+    private Vector3 correctPlayerPos;
+    private Quaternion correctPlayerRot = Quaternion.identity; // We lerp towards this
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        // Check to see if this NetworkPlayer is the owned by the current instance
+        if (!photonView.isMine)
+        {
+            // Lerping smooths the movement
+            transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+            otherPlayerHead.transform.rotation = Quaternion.Lerp(otherPlayerHead.transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(playerCamera.transform.rotation);
+        }
+        else
+        {
+            // Network player, receive data
+            this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+            this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject head;
+    public GameObject playerCamera;
+
+    // Used to check if is this user's player or an external player
+    public bool isControllable;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isControllable)
+        {
+            playerCamera.SetActive(true);
+            head.SetActive(false);
+        }
+        else
+        {
+            playerCamera.SetActive(false);
+        }
+    }
+}
