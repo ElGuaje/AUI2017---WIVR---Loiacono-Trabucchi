f0b3258885f5ff1219813bb7eed0b0e9e211834b
Unknown <tommaso.loiacono01@gmail.com>
2018-04-06 15:40:37 +0200

    Setting scene up

---
+++ Assets/Scripts/CamerinusScripts/AvatarSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSelection : MonoBehaviour {
+
+    public void SetGazedAt(bool isGazed)
+    {
+        if (isGazed)
+        {
+            StartCoroutine("StartChangingOutfit");
+        }
+        else
+        {
+            StopCoroutine("StartChangingOutfit");
+        }
+    }
+
+    IEnumerator StartChangingOutfit()
+    {
+        yield return new WaitForSeconds(5);
+    }
+}
